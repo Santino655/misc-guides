38bd112af58d905751cb4ed04b8490907551f68d
IronThread <fsantino676@gmail.com>
2026-08-30 00:12:09 -0300

    fix jsr

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import deno from '@deno/vite-plugin';
 
 export default defineConfig({
   base: "/misc-guides/",
-  plugins: [react()],
+  plugins: [
+    deno(),
+    react(),
+  ],
   build: {
     outDir: "dist",
   },
